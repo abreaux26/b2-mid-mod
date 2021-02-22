9d23cf2f68cfbe47a7e21af9bdb070fa55074034
Angel Breaux <abreaux@detaso.com>
2021-02-22 11:27:59 -0600

    Update seeds file and fix show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,10 @@
 
 @kara = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
 @angel = Mechanic.create!(name: 'Angel Breaux', years_experience: 5)
+
+@outlaw = @kara.rides.create!(name: 'Outlaw Run', thrill_rating: 6, open: true)
+@time_traveler = @kara.rides.create!(name: 'Time Traveler', thrill_rating: 10, open: true)
+@fire_in_the_hole = @kara.rides.create!(name: 'Fire in the Hole', thrill_rating: 4, open: false)
+
+@thunderation = @angel.rides.create!(name: 'Thunderation', thrill_rating: 4, open: false)
+@wildfire = @angel.rides.create!(name: 'Wildfire', thrill_rating: 7, open: true)
