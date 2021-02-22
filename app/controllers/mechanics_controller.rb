f1063d00c66bc5d8a99bd3da6d352f2394f0b437
Angel Breaux <abreaux@detaso.com>
2021-02-22 11:25:31 -0600

    Add show method to mechanics conroller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,9 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @mechanic_rides = @mechanic.rides.order_by_thrill_rating
+  end
 end
